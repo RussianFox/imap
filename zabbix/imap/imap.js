c25ebe5cfdfdfbfb654d709d26018741a76ecb13
RussianFox <horonitel@gmail.com>
2017-09-13 19:28:32 +0300

    Layers change (https)

--- zabbix/imap/imap.js
+++ zabbix/imap/imap.js
@@ -38,10 +38,10 @@
 		var baseMaps = new Object;
 		var overlayMaps = new Object;
 		
-		baseMaps["OpenStreetMap"] = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom:18, attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
-		baseMaps["OpenCycleMap"] = new L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {maxZoom:18, attribution: 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
+		baseMaps["OpenStreetMap"] = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom:18, attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
+		baseMaps["OpenCycleMap"] = new L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {maxZoom:18, attribution: 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
 		baseMaps["Stamen B&W"] = new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {maxZoom:18, subdomains:'abcd', attribution: 'Map Data: © <a href="http://maps.stamen.com/" target="_blank">Stamen.com</a>'});
-		baseMaps["Kosmosnimki.ru OSM"] = new L.tileLayer('http://{s}.tile.osm.kosmosnimki.ru/kosmo/{z}/{x}/{y}.png', {maxZoom:18, subdomains:'abcdef', attribution: 'Map Data: © <a href="http://osm.kosmosnimki.ru/" target="_blank">osm.kosmosnimki.ru</a>'});
+		baseMaps["Kosmosnimki.ru OSM"] = new L.tileLayer('https://tilessputnik.ru/{z}/{x}/{y}.png', {maxZoom:18, subdomains:'abcdef', attribution: 'Map Data: © <a href="http://osm.kosmosnimki.ru/" target="_blank">osm.kosmosnimki.ru</a>'});
 		
 		if (bingAPIkey) {
 			baseMaps["Bing Satellite"] = new L.BingLayer(bingAPIkey, {culture: _imap.settings.lang, type: 'Aerial'});
